f61072daf1394e04e7edd1153132f2a5f235db6d
bradenryan <braden.ross.ryan@gmail.com>
2016-11-03 16:38:40 +0800

    Add files via upload

---
+++ header.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int check_type(char *pathname);
+
+void list_directory(char *dirname, bool aflag, bool lflag, char *options);
+
+void list_directory_stat_exp(char *dirname, bool aflag, bool lflag, char *options);
+
+void list_directory_depth(char *dirname, bool aflag, bool lflag, int depth, char options);
+
+int count_all(char *dirname, bool aflag, int filecount);
+
+void unlink_dir(char *pathname, bool aflag);
+
+int read_args (int argc, char *argv[]);
